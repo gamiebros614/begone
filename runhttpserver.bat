2ca53809c2a5244a01b72b1ca79713e46d4266a4
classroom6x1945 <gghd6f0z@gmail.com>
2023-11-11 18:44:02 +0700

    20231111

---
+++ runhttpserver.bat
@@ -0,0 +1,3 @@
+rem http-server --cors -p 8089 -S -C ../celenganku.github.io/cert.pem -K ../celenganku.github.io/key.pem
+http-server --cors -p 8086
+rem http-server --cors -p 8086 -S -C cert.pem -K key.pem
